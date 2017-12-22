aa49b024493073b3da99999e7b04af8efbd2c370
Jonathanghl <31654288+Jonathanghl@users.noreply.github.com>
2017-12-22 01:53:53 +0100

    Fået kryptering til at virke og puttet det ind i SDK

--- javascript-client/js/sdk.js
+++ javascript-client/js/sdk.js
@@ -1,79 +1,76 @@
 const SDK = {
 
-/*Dette er vores SDK klasse. Den er afgørende for vores program. Det er her der laves ajax kald
-* til serveren. Delt op i QUiz, question, course, choice, user og storage, som gør local sotrage brug
-* bart for os*/
-  serverURL: "http://localhost:8080/api",
-  request: (options, cb) => {
+    /*Dette er vores SDK klasse. Den er afgørende for vores program. Det er her der laves ajax kald
+    * til serveren. Delt op i QUiz, question, course, choice, user og storage, som gør local sotrage brug
+    * bart for os*/
+    serverURL: "http://localhost:8080/api",
+    request: (options, cb) => {
 
 
-    $.ajax({
-      url: SDK.serverURL + options.url,
-      method: options.method,
-      contentType: "application/json",
-      dataType: "json",
-      data: JSON.stringify(options.data),
-      success: (data, status, xhr) => {
-        cb(null, data, status, xhr);
-      },
-      error: (xhr, status, errorThrown) => {
-        cb({xhr: xhr, status: status, error: errorThrown});
-      }
-    });
+        $.ajax({
+            url: SDK.serverURL + options.url,
+            method: options.method,
+            contentType: "application/json",
+            dataType: "json",
+            data: JSON.stringify(SDK.Encryption.encrypt(JSON.stringify(options.data))),
+            success: (data, status, xhr) => {
+                cb(null, (SDK.Encryption.decrypt(data)), status, xhr);
+            },
+            error: (xhr, status, errorThrown) => {
+                cb({xhr: xhr, status: status, error: errorThrown});
+            }
+        });
 
-  },
-  Quiz: {
+    },
+    Quiz: {
 
-      findAll: (id, cb) => {
-          SDK.request({
-              method: "GET",
-              url: "/quiz/" + id,
+        findAll: (id, cb) => {
+            SDK.request({
+                method: "GET",
+                url: "/quiz/" + id,
 
-          }, (err, data) => {
-              if (err) return cb(err);
+            }, (err, data) => {
+                if (err) return cb(err);
 
-              data = JSON.parse(data);
+                data = JSON.parse(data);
 
-              cb(null, data);
-          });
-      },
-      create: (quizTitle, courseId, cb) => {
-          SDK.request({
-              method: "POST",
-              url: "/quiz",
-              data: {
+                cb(null, data);
+            });
+        },
+        create: (quizTitle, courseId, cb) => {
+            SDK.request({
+                method: "POST",
+                url: "/quiz",
+                data: {
 
-                  quizTitle: quizTitle,
-                  courseId: courseId
+                    quizTitle: quizTitle,
+                    courseId: courseId
 
-              },
+                },
 
-          }, (err, data) => {
+            }, (err, data) => {
 
-              if (err) return cb(err);
+                if (err) return cb(err);
 
-              cb(null, data);
+                cb(null, data);
 
-          });
-      },
+            });
+        },
 
-      delete: (id, cb) => {
-          SDK.request({
-                  method: "DELETE",
-                  url: "/quiz/" + id,
+        delete: (id, cb) => {
+            SDK.request({
+                    method: "DELETE",
+                    url: "/quiz/" + id,
 
-              },
-              (err) => {
+                },
+                (err) => {
 
-                  if (err) return cb(err);
-                  cb(null);
+                    if (err) return cb(err);
+                    cb(null);
 
-              });
-      },
-  },
-
-
-
+                });
+        },
+    },
 
 
     Question: {
@@ -114,85 +111,83 @@ const SDK = {
     },
 
 
-  Course: {
-    findAll: (cb) => {
-        SDK.request({
-            method: "GET",
-            url: "/courses"
+    Course: {
+        findAll: (cb) => {
+            SDK.request({
+                method: "GET",
+                url: "/courses"
 
-        }, (err, data) => {
-            if (err) return cb(err);
+            }, (err, data) => {
+                if (err) return cb(err);
 
-            data = JSON.parse(data);
-            cb(null, data);
-        });
-    }
+                data = JSON.parse(data);
+                cb(null, data);
+            });
+        }
 
     },
 
 
+    Choice: {
+
+        create: (choiceTitle, questionId, answer, cb) => {
+            SDK.request({
+                method: "POST",
+                url: "/choice",
+                data: {
+                    questionId: questionId,
+                    choiceTitle: choiceTitle,
+                    answer: answer
 
 
-  Choice: {
+                },
 
-      create: (choiceTitle, questionId, answer, cb) => {
-          SDK.request({
-              method: "POST",
-              url: "/choice",
-              data: {
-                  questionId: questionId,
-                  choiceTitle: choiceTitle,
-                  answer: answer
+            }, (err, data) => {
 
+                if (err) return cb(err);
 
-              },
+                cb(null, data);
 
-          }, (err, data) => {
+            });
+        },
+        findAll: (id, cb) => {
+            SDK.request({
+                method: "GET",
+                url: "/choice/" + id,
 
-              if (err) return cb(err);
+            }, (err, data) => {
+                if (err) return cb(err);
 
-              cb(null, data);
+                data = JSON.parse(data);
 
-          });
-      },
-      findAll: (id, cb) => {
-          SDK.request({
-              method: "GET",
-              url: "/choice/" + id,
-
-          }, (err, data) => {
-              if (err) return cb(err);
-
-              data = JSON.parse(data);
-
-              cb(null, data);
-          });
-      },
-  },
-
-
-  User: {
-    delete: (id, cb) => {
-          SDK.request({
-                  method: "DELETE",
-                  url: "/user/" + id,
-
-              },
-              (err) => {
-
-                  if (err) return cb(err);
-                  cb(null);
-
-              });
-      },
-
-    findAll: (cb) => {
-      SDK.request({method: "GET", url: "/user"}, cb);
+                cb(null, data);
+            });
+        },
     },
 
-    current: () => {
-      return {
-            username: SDK.Storage.load("user"),
+
+    User: {
+        delete: (id, cb) => {
+            SDK.request({
+                    method: "DELETE",
+                    url: "/user/" + id,
+
+                },
+                (err) => {
+
+                    if (err) return cb(err);
+                    cb(null);
+
+                });
+        },
+
+        findAll: (cb) => {
+            SDK.request({method: "GET", url: "/user"}, cb);
+        },
+
+        current: () => {
+            return {
+                username: SDK.Storage.load("user"),
 
                 userId: SDK.Storage.load("userId"),
 
@@ -202,138 +197,170 @@ const SDK = {
 
                 type: SDK.Storage.load("userType")
 
-        }
+            }
 
 
+        },
+        logOut: () => {
+            SDK.Storage.remove("QuestionId");
+            SDK.Storage.remove("QuestionTitle");
+            SDK.Storage.remove("QuizId");
+            SDK.Storage.remove("courseId");
+            SDK.Storage.remove("deleteUserId");
+            SDK.Storage.remove("firstname");
+            SDK.Storage.remove("lastname");
+            SDK.Storage.remove("userType");
+            SDK.Storage.remove("userId");
+            SDK.Storage.remove("user");
+            SDK.Storage.remove("watchQuizCourseId");
+            window.location.href = "login.html";
+        },
+        login: (username, password, cb) => {
+            SDK.request({
+                url: "/user/login",
+                method: "POST",
+                data: {
 
-    },
-    logOut: () => {
-        SDK.Storage.remove("QuestionId");
-        SDK.Storage.remove("QuestionTitle");
-        SDK.Storage.remove("QuizId");
-        SDK.Storage.remove("courseId");
-        SDK.Storage.remove("deleteUserId");
-        SDK.Storage.remove("firstname");
-        SDK.Storage.remove("lastname");
-        SDK.Storage.remove("userType");
-        SDK.Storage.remove("userId");
-        SDK.Storage.remove("user");
-        SDK.Storage.remove("watchQuizCourseId");
-      window.location.href = "login.html";
-    },
-    login: (username, password, cb) => {
-      SDK.request({
-          url: "/user/login",
-          method: "POST",
-          data: {
+                    username: username,
+                    password: password
+                },
 
-          username: username,
-          password: password
+            }, (err, data) => {
+
+                if (err) return cb(err);
+                data = JSON.parse(data);
+                SDK.Storage.persist("userType", data.type);
+                SDK.Storage.persist("user", data.username);
+                SDK.Storage.persist("userId", data.userId),
+                    SDK.Storage.persist("firstname", data.firstName),
+                    SDK.Storage.persist("lastname", data.lastName),
+
+                    cb(null, data);
+
+            });
+        },
+        create: (username, password, firstname, lastname, cb) => {
+            SDK.request({
+                url: "/user",
+                method: "POST",
+                data: {
+
+                    username: username,
+                    password: password,
+                    firstName: firstname,
+                    lastName: lastname,
+                    type: 1
+                },
+
+            }, (err, data) => {
+
+                if (err) return cb(err);
+
+                cb(null, data);
+
+            });
         },
 
-      }, (err, data) => {
+        createAdmin: (username, password, firstname, lastname, cb) => {
+            SDK.request({
+                url: "/user",
+                method: "POST",
+                data: {
 
-        if (err) return cb(err);
-          data = JSON.parse(data);
-        SDK.Storage.persist("userType", data.type);
-        SDK.Storage.persist("user", data.username);
-        SDK.Storage.persist("userId", data.userId),
-            SDK.Storage.persist("firstname", data.firstName),
-            SDK.Storage.persist("lastname", data.lastName),
+                    username: username,
+                    password: password,
+                    firstName: firstname,
+                    lastName: lastname,
+                    type: 2
+                },
 
-        cb(null, data);
+            }, (err, data) => {
 
-      });
-    },
-      create: (username, password, firstname, lastname, cb) => {
-          SDK.request({
-              url: "/user",
-              method: "POST",
-              data: {
+                if (err) return cb(err);
 
-                  username: username,
-                  password: password,
-                  firstName: firstname,
-                  lastName: lastname,
-                  type: 1
-              },
+                cb(null, data);
 
-          }, (err, data) => {
+            });
+        },
 
-              if (err) return cb(err);
-
-              cb(null, data);
-
-          });
-      },
-
-      createAdmin: (username, password, firstname, lastname, cb) => {
-          SDK.request({
-              url: "/user",
-              method: "POST",
-              data: {
-
-                  username: username,
-                  password: password,
-                  firstName: firstname,
-                  lastName: lastname,
-                  type: 2
-              },
-
-          }, (err, data) => {
-
-              if (err) return cb(err);
-
-              cb(null, data);
-
-          });
-      },
-
-    loadNav: (cb) => {
-      $("#nav-container").load("nav.html", () => {
-        const currentUser = SDK.User.current();
-        if (currentUser.userId !== null) {
-          $(".navbar-right").html(`
+        loadNav: (cb) => {
+            $("#nav-container").load("nav.html", () => {
+                const currentUser = SDK.User.current();
+                if (currentUser.userId !== null) {
+                    $(".navbar-right").html(`
             <li><a href="#" id="logout-link">Log ud</a></li>
           `);
-          if (currentUser.type == 2){
-            $(".navbar-left").append(`
-            <li><a href="my-page.html">Dine fag</a></li>`);}
-            else {$(".navbar-left").append(`
-            <li><a href="courseForQuiz.html">Dine fag</a></li>`);}
-        } else {
-          $(".navbar-right").html(`
+                    if (currentUser.type == 2) {
+                        $(".navbar-left").append(`
+            <li><a href="my-page.html">Dine fag</a></li>`);
+                    }
+                    else {
+                        $(".navbar-left").append(`
+            <li><a href="courseForQuiz.html">Dine fag</a></li>`);
+                    }
+                } else {
+                    $(".navbar-right").html(`
             <li><a href="login.html">Log ind <span class="sr-only">(current)</span></a></li>
           `);
+                }
+                $("#logout-link").click(() => SDK.User.logOut());
+                cb && cb();
+            });
         }
-        $("#logout-link").click(() => SDK.User.logOut());
-        cb && cb();
-      });
-    }
-  },
-
-
-  Storage: {
-    prefix: "ExamQuizSDK",
-    persist: (key, value) => {
-      window.localStorage.setItem(SDK.Storage.prefix + key, (typeof value === 'object') ? JSON.stringify(value) : value)
     },
-    load: (key) => {
-      const val = window.localStorage.getItem(SDK.Storage.prefix + key);
-      try {
-        return JSON.parse(val);
-      }
-      catch (e) {
-        return val;
-      }
+
+
+    Storage: {
+        prefix: "ExamQuizSDK",
+        persist: (key, value) => {
+            window.localStorage.setItem(SDK.Storage.prefix + key, (typeof value === 'object') ? JSON.stringify(value) : value)
+        },
+        load: (key) => {
+            const val = window.localStorage.getItem(SDK.Storage.prefix + key);
+            try {
+                return JSON.parse(val);
+            }
+            catch (e) {
+                return val;
+            }
+        },
+        remove: (key) => {
+            window.localStorage.removeItem(SDK.Storage.prefix + key);
+        }
     },
-    remove: (key) => {
-      window.localStorage.removeItem(SDK.Storage.prefix + key);
+
+    Encryption: {
+        decrypt: (willBeDecrypted) => {
+
+            if (willBeDecrypted !== undefined && willBeDecrypted.length !== 0) {
+                const key = ["K", "O", "C", "H"];
+                let nowDecrypted = "";
+                for (let i = 0; i < willBeDecrypted.length; i++) {
+                    nowDecrypted += (String.fromCharCode((willBeDecrypted.charAt(i)).charCodeAt(0) ^ (key[i % key.length]).charCodeAt(0)))
+                }
+
+                return nowDecrypted;
+            }
+            else {
+                return willBeDecrypted;
+            }
+        },
+
+    encrypt: (willBeEncrypted) => {
+
+        if (willBeEncrypted !== undefined && willBeEncrypted.length !== 0) {
+            const key = ["K", "O", "C", "H"];
+            let nowEncrypted = "";
+            for (let i = 0; i < willBeEncrypted.length; i++) {
+                nowEncrypted += (String.fromCharCode((willBeEncrypted.charAt(i)).charCodeAt(0) ^ (key[i % key.length]).charCodeAt(0)))
+            }
+            return nowEncrypted
+        }
+        else {
+            return willBeEncrypted
+        }
     }
-  },
-
-
-
+}
 
 
 
